0c3d77cd2da7a1510d6ea9ab6231368374e2e1bc
jelena2803 <jelenaiva2803@gmail.com>
2024-01-11 00:15:57 +0200

    Footer component created and styled

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function Footer() {
+    return (
+      <footer>
+        <p>Copyright 2024 | Github</p>
+      </footer>
+          
+        
+    );
+}
+
+export default Footer;
