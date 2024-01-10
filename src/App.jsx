4cc439b57e02291486ae9e641d328dadc3b76905
jelena2803 <jelenaiva2803@gmail.com>
2024-01-11 00:27:00 +0200

    changed extension name from js to jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
