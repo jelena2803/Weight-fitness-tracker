ae1d3fb9bb919d219b00bdaee2e4eb06903df464
jelena2803 <jelenaiva2803@gmail.com>
2024-01-11 00:47:36 +0200

    paths for Navbar and Footer files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
