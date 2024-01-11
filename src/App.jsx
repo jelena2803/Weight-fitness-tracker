428596b9d5576b49282ba9b2c0b18ba7b141a0bd
jelena2803 <jelenaiva2803@gmail.com>
2024-01-11 09:14:38 +0200

    Router installed, router paths for signup, login and completeprofile components are created and tested

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,31 @@ import Footer from "./components/Footer";
 import CompleteProfile from "./components/CompleteProfile";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Signup/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/newprofile",
+    element: <CompleteProfile/>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <CompleteProfile/>
-      <Login/>
-      <Signup/>
+      <RouterProvider router={router} />
       <Footer />
     </div>
   );
