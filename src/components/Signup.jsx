c3c4a253334c415e8e69c143b8760831197efccd
jelena2803 <jelenaiva2803@gmail.com>
2024-01-11 02:35:03 +0200

    elements added and the layout is tested, functionality to be done

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,11 +2,20 @@ import React from "react";
 
 function Signup() {
     return (
-<div>
-    <input type="email" placeholder="Insert your email"/>
-    <input type="password" placeholder="Insert your password"/>
-    <button>Sign up</button>
-</div>
+            <div className="profile-form">
+                <div className="init-profile-form">
+                    <div>
+                        <label htmlFor="email">Your email:</label>
+                        <input type="email" id="email" placeholder="Enter your email" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password" placeholder="Enter your password" />
+                    </div>
+                </div>
+
+                <button>Sign up</button>
+            </div>
     );
 }
 
