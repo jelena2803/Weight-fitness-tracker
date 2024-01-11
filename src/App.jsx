3e00f62b6c59bd9a7875afbacb9a3fa1efa8a8eb
jelena2803 <jelenaiva2803@gmail.com>
2024-01-11 02:32:43 +0200

    components added temporarily for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import CompleteProfile from "./components/CompleteProfile";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <CompleteProfile/>
+      <Login/>
+      <Signup/>
       <Footer />
     </div>
   );
