c94f1c38cc51a5071d7ed56cfa7b7029a910095e
jelena2803 <jelenaiva2803@gmail.com>
2024-01-12 02:10:02 +0200

    imported BrowserRouter and rendered

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+// import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav>
-        <ul>
-          <li className="menu-left">Weight and Fitness Tracker</li>
-          <span className="menu-right">
-            <li>Profile</li>
-            <li>Weight</li>
-            <li>Fitness</li>
-            <li>Nutrition</li>
-            <li>Log in</li>
-          </span>
-        </ul>
-      </nav>
-    );
+  return (
+    <nav>
+      <ul>
+        <li className="menu-left">Weight and Fitness Tracker</li>
+        <span className="menu-right">
+          <li> <NavLink to={"/profile"} className={({isActive}) =>
+    isActive ? "active" : "inactive"}> Profile </NavLink></li>
+          <li>Weight</li>
+          <li>Fitness</li>
+          <li>Nutrition</li>
+          <li> <NavLink to={"/login"} className={({isActive}) =>
+    isActive ? "active" : "inactive"}>Log in</NavLink>  </li>
+        </span>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
