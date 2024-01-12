33ae20556d575805ca64001643d7e5c47cb60d3f
jelena2803 <jelenaiva2803@gmail.com>
2024-01-12 02:14:18 +0200

    BrowserRouter, createBrowserRouter and RouterProvider replaced with Route and Routes, navbar and pages responsiveness tested

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,46 @@ import CompleteProfile from "./components/CompleteProfile";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Profile from "./components/Profile";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-// import { Link } from "react-router-dom";
+// import {
+//   BrowserRouter, createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+import { Route,Routes } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Signup/>,
-  },
-  {
-    path: "/login",
-    element: <Login/>,
-  },
-  {
-    path: "/newprofile",
-    element: <CompleteProfile/>,
-  },
-  {
-    path: "/profile",
-    element: <Profile/>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Signup />,
+//   },
+//   {
+//     path: "/login",
+//     element: <Login />,
+//   },
+//   {
+//     path: "/newprofile",
+//     element: <CompleteProfile />,
+//   },
+//   {
+//     path: "/profile",
+//     element: <Profile />,
+//   },
+// ]);
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <RouterProvider router={router} />
+      {/* <BrowserRouter> */}
+        <Navbar />
+      {/* </BrowserRouter> */}
+      <div>
+    <Routes>        
+      <Route path="/"  element={<Signup/>} />
+      <Route path="/login"  element={<Login />}/>
+      <Route path="/newprofile"  element={<CompleteProfile/>}/>
+      <Route path="/profile"  element={<Profile/>}/>
+    </Routes>
+    </div>
+      {/* <RouterProvider router={router} /> */}
       <Footer />
     </div>
   );
