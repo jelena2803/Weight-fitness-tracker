1227ecdd44145282ee3f1eb4ce17f51b7dc1e0cc
jelena2803 <jelenaiva2803@gmail.com>
2024-01-14 03:35:50 +0200

    weightList for data storage created and useEffect added to fetch and render the data from backend, tested, the requested data are displayed in the browser

--- src/components/Weight.jsx
+++ src/components/Weight.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import axios from "axios";
+import { useEffect , useState} from "react";
 
 function Weight() {
+    let [weightList, setWeightList ] = useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:3636/weight").then(({data}) => {
+            console.log(data);
+            setWeightList(data);
+        })
+    }, []);
+    
     return (
         <div className="profile-form ">
 
@@ -14,10 +24,20 @@ function Weight() {
                 <p className="item title">Weight</p>
                 <p className="item title">BMI</p>
                 <hr />
+
+                {weightList.map((e)=> {
+                    return (
+                        <> <p className="item ">{e.date.split("T")[0]}</p>
+                        <p className="item ">{e.weight}</p>
+                        <p className="item ">{e.BMI}</p>
+                        </>
+                    )
+                }
+                )}
             </div>
 
         </div>
     )
 }
 
-export default Weight
+export default Weight;
