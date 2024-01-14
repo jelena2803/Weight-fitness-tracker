7e19f686e90b65f4a90e483497ede10320c177da
jelena2803 <jelenaiva2803@gmail.com>
2024-01-14 02:16:23 +0200

    NewWeight and NewActivity components imported

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import NewActivity from "./NewActivity";
+import NewWeight from "./NewWeight";
 
 function Profile() {
     return (
@@ -7,15 +9,23 @@ function Profile() {
             <p className="profile-data">Gender:</p>
             <p className="profile-data">Age:</p>
             <p className="profile-data">Height:</p>
+            
+            <hr />
 
             <div className="add-new-data">
+                <NewWeight />
                 <button>Add weight</button>
-                <button>Add fitness activity</button>
+                <button>My weight log</button>
+
+                <hr />
+
+                <NewActivity />
+                <button>Add new activity</button>
+                <button>My activities</button>
             </div>
-
-
         </div>
+
     )
 }
 
-export default Profile
+export default Profile;
