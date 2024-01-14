a1c349cdaccaa0f1279e5e6aa22c30caac38a48b
jelena2803 <jelenaiva2803@gmail.com>
2024-01-14 03:32:59 +0200

    fitnessList for data storage created and useEffect added to fetch and render the data from backend, tested, the requested data are displayed in the browser

--- src/components/Fitness.jsx
+++ src/components/Fitness.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import axios from "axios";
+import { useEffect , useState } from "react";
 
 function Fitness() {
+    let [fitnessList, setFitnessList ] = useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:3636/fitness").then(({data}) => {
+            console.log(data);
+            setFitnessList(data);
+        })
+    }, []);
+
     return (
         <div className="profile-form">
             <div className="add-new-data">
@@ -9,11 +19,22 @@ function Fitness() {
             </div>
 
             <div className="fitness-container">
+        
                 <p className="item title">Date</p>
                 <p className="item title">Activity</p>
-                <p className="item title">Duration</p>
+                <p className="item title">Duration (in minutes)</p>
                 {/* <p className="fitness-data">Delete button</p> */}
                 <hr />
+
+                {fitnessList.map((e)=> {
+                    return (
+                        <> <p className="item ">{e.date.split("T")[0]}</p>
+                        <p className="item ">{e.activity}</p>
+                        <p className="item ">{e.duration}</p>
+                        </>
+                    )
+                }
+                )}
             </div>
 
         </div>
