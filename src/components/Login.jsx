a9e063d1fccbb45eea0d08e07820eec53f983eda
jelena2803 <jelenaiva2803@gmail.com>
2024-01-15 15:56:58 +0200

    Login component completed and tested

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    function login() {
+        axios.post("http://localhost:3636/login", {username : username, password : password,})
+        .then(({data}) => {
+            if (data.msg === "Successful login") {
+                //redirect the user to the login page
+                navigate("/profile");
+            } else {
+                setError(data.msg);
+            }
+        })
+    }
+
+
     return (
             <div className="profile-form">
                 <div className="init-profile-form">
                     <div>
                         <label htmlFor="username">Your username:</label>
-                        <input type="text" id="username" placeholder="Enter your username" />
+                        <input type="text" id="username" placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)}/>
                     </div>
                     <div>
                         <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" placeholder="Enter your password" />
+                        <input type="password" id="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                 </div>
-
-                <button>Log in</button>
+                <p>{error}</p>
+                <button onClick={login}>Log in</button>
 
                 <p>Don't have an account? You need to <Link to={`/`}>sign up</Link>.</p>
             </div>
