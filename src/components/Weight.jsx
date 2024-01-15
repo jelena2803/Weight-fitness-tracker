55edf8d1b1a1ebaab7883c768dcd6ddfda5b3e11
jelena2803 <jelenaiva2803@gmail.com>
2024-01-15 03:32:07 +0200

    added functions to send current date, weight and bmi and render them in the page

--- src/components/Weight.jsx
+++ src/components/Weight.jsx
@@ -1,35 +1,79 @@
 import React from "react";
 import axios from "axios";
-import { useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 
 function Weight() {
-    let [weightList, setWeightList ] = useState([]);
+
+let [ weightList, setWeightList ] = useState([]);
+let [ currentDate, setCurrentDate ] = useState("");
+let [ currentWeight, setCurrentWeight ] = useState(0);
+let [ BMI, setBMI ] = useState(0);
+ 
+function getWeight(){
+    axios.get("http://localhost:3636/weight").then(({data}) => {
+        console.log(data);
+        setWeightList(data);
+    })
+}
+
+function calcBMI() {
+    let kg = currentWeight;
+    let result = (kg / ((152 * 152)/1000)).toFixed(2);
+    setBMI(result);
+}
+
+function getCurrentDate() {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const today = new Date();
+    const month = months[today.getMonth()];
+    //   const monthNum = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+    setCurrentDate(month + " " + date + " " + year);
+    return currentDate;
+  }
+
+function addNewWeight(){
+    calcBMI();
+    getCurrentDate();
+    axios.post("http://localhost:3636/weight", {
+        date: currentDate,
+        weight: currentWeight,
+        BMI: BMI,
+    }).then((data)=>{
+        console.log(data);
+        getWeight();
+    })}
+
     useEffect(()=>{
-        axios.get("http://localhost:3636/weight").then(({data}) => {
-            console.log(data);
-            setWeightList(data);
-        })
+        getWeight();
     }, []);
     
     return (
         <div className="profile-form ">
 
             <div className="add-new-data">
-                <button>Add weight</button>
-                <button>Add fitness activity</button>
+            <div className="init-profile-form">
+                <div>
+                    <label htmlFor="weight">My new weight:</label>
+                    <input type="number" id="weight" placeholder="60" step="1" onChange={(e)=>setCurrentWeight(e.target.value)}/>
+                </div>
+            </div>
+                <button onClick={() => { addNewWeight() }}>Add weight</button>
+                {/* <button>Add fitness activity</button> */}
             </div>
 
             <div className="weight-container">
                 <p className="item title">Date</p>
-                <p className="item title">Weight</p>
+                <p className="item title">Weight (kg)</p>
                 <p className="item title">BMI</p>
                 <hr />
 
                 {weightList.map((e)=> {
                     return (
-                        <> <p className="item ">{e.date.split("T")[0]}</p>
-                        <p className="item ">{e.weight}</p>
-                        <p className="item ">{e.BMI}</p>
+                        <> <p className="item">{e.date}</p>
+                        <p className="item">{e.weight}</p>
+                        <p className="item">{e.BMI}</p>
                         </>
                     )
                 }
