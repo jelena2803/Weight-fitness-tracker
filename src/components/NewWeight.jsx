902a1cf0485f4dc7620072b83d4c7b18a4fad8f0
jelena2803 <jelenaiva2803@gmail.com>
2024-01-15 03:15:09 +0200

    functionality temporarily paused for this component

--- src/components/NewWeight.jsx
+++ src/components/NewWeight.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+// import { useState } from "react";
+import axios from "axios";
 
 function NewWeight() {
 
-    const [newWeight, setNewWeight] = useState(null);
-
-    // function addNewWeight(){
-
+    // function addNewWeight() {
+    //     axios.post("http://localhost:3636/weight", {
+    //         weight: 32,
+    //         BMI: null,
+    //     }).then((data) => {
+    //         console.log(data);
+    //         // getWeight();
+    //     })
     // }
 
     return (
@@ -13,8 +19,11 @@ function NewWeight() {
             <div className="init-profile-form">
                 <div>
                     <label htmlFor="weight">My new weight:</label>
-                    <input type="number" id="weight" placeholder="60" step="5" /></div>
+                    <input type="number" id="weight" placeholder="60" step="5" />
+                </div>
             </div>
+            <button >Add weight</button>
+            <button>My weight log</button>
         </>
 
     )
