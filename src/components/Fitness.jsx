62124bd84c1cca0040a12d877b0533061aa3aa09
jelena2803 <jelenaiva2803@gmail.com>
2024-01-16 17:09:30 +0200

    component completed and tested

--- src/components/Fitness.jsx
+++ src/components/Fitness.jsx
@@ -1,36 +1,116 @@
 import React from "react";
 import axios from "axios";
-import { useEffect , useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Fitness() {
-    let [fitnessList, setFitnessList ] = useState([]);
-    useEffect(()=>{
-        axios.get("http://localhost:3636/fitness").then(({data}) => {
-            console.log(data);
-            setFitnessList(data);
-        })
+    const navigate = useNavigate();
+    const [user, setUser] = useState({});
+    const [fitnessList, setFitnessList] = useState([]);
+    let token = localStorage.getItem("token");
+    const headers = {headers : {Authorization : "Bearer " + localStorage.getItem("token") },};
+    const [activity, setActivity] = useState("");
+    const [time, setTime] = useState(0);
+    let [currentDate, setCurrentDate] = useState("");
+    // const [error, setError] = useState("");
+
+    function getActivities() {
+        try {
+            axios.get("http://localhost:3636/fitness", { headers: { Authorization: `Bearer ${token}` } })
+                .then(({ data }) => {
+                    setFitnessList(data);
+                    console.log(fitnessList);
+                })
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem("user")) {
+            setUser(JSON.parse(localStorage.getItem("user")));
+            getActivities();
+            // axios.get("http://localhost:3636/weight" + user._id).then(({ data }) => {
+            //     console.log(data);})
+        } else {
+            navigate("/login");
+        }
     }, []);
+
+    function getCurrentDate() {
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const today = new Date();
+        const month = months[today.getMonth()];
+        //   const monthNum = today.getMonth() + 1;
+        const year = today.getFullYear();
+        const date = today.getDate();
+        setCurrentDate(month + " " + date + " " + year);
+        return currentDate;
+    }
+
+    function handleInputChange (e) {
+        setActivity(e.target.value);
+        getCurrentDate();
+    }
+
+    function handleTimeInput (e) {
+        setTime(e.target.value);
+        getCurrentDate();
+    }
+
+
+    function addActivity() {
+        axios.post("http://localhost:3636/fitness", {
+            userId: user._id,
+            date: currentDate,
+            activity : activity, 
+            duration : time,
+        }, headers ).then(({data}) => {
+            if (data) {console.log("new activity sent");
+            getActivities();
+        } else {
+                console.log(" activity not sent")
+            }
+            
+        })
+    }
+
 
     return (
         <div className="profile-form">
             <div className="add-new-data">
-                <button>Add weight</button>
-                <button>Add fitness activity</button>
+
+                <div className="init-profile-form">
+                    <div>
+                        <label htmlFor="activity">New activity:</label>
+                        <input type="text" id="activity" placeholder="Enter new activity" onChange={(e) => handleInputChange(e)}  />
+                    </div>
+
+                    <div>
+                        <label htmlFor="duration">Duration in minutes:</label>
+                        <input type="number" id="duration" value={time}  placeholder="30" step="5" onChange={(e) => handleTimeInput(e)}/>
+                    </div>
+                </div>
+
+                <button onClick={() => { addActivity() }}>Add activity</button>
+                <button> <NavLink to={"/weight"} className={"logs"}> My weight log </NavLink> </button>
             </div>
 
             <div className="fitness-container">
-        
+
                 <p className="item title">Date</p>
                 <p className="item title">Activity</p>
                 <p className="item title">Duration (in minutes)</p>
                 {/* <p className="fitness-data">Delete button</p> */}
                 <hr />
 
-                {fitnessList.map((e)=> {
+                {fitnessList.map((e) => {
                     return (
-                        <> <p className="item ">{e.date.split("T")[0]}</p>
-                        <p className="item ">{e.activity}</p>
-                        <p className="item ">{e.duration}</p>
+                        <> <p className="item ">{e.date}</p>
+                            <p className="item ">{e.activity}</p>
+                            <p className="item ">{e.duration}</p>
                         </>
                     )
                 }
