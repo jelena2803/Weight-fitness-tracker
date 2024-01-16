0f89b58d7f1f49a2717bf940abce7a294fdfd7c3
jelena2803 <jelenaiva2803@gmail.com>
2024-01-16 17:12:05 +0200

    component completed and tested

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,41 +1,59 @@
 import React from "react";
-import NewActivity from "./NewActivity";
-import NewWeight from "./NewWeight";
+// import NewActivity from "./NewActivity";
+// import NewWeight from "./NewWeight";
+// import axios from "axios";
+import { useState, useEffect } from "react";
+import { NavLink, useNavigate} from "react-router-dom";
+
 
 function Profile() {
+    const navigate = useNavigate();
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        if (localStorage.getItem("user")) {
+            setUser(JSON.parse(localStorage.getItem("user")));
+        } else {
+            navigate("/login");
+        }
+    }, []);
+
     return (
         <div className="profile-form">
-            <p className="profile-data">Username:</p>
-            <p className="profile-data">Gender:</p>
-            <p className="profile-data">Age:</p>
-            <p className="profile-data">Height:</p>
-            
+            <p className="profile-data">Username:  {user.username}</p>
+            <p className="profile-data">Gender:  {user.gender}</p>
+            <p className="profile-data">Age:  {user.age}</p>
+            <p className="profile-data">Height:  {user.height}</p>
+
             <hr />
 
             <div className="add-new-data">
-                <NewWeight />
+                {/* <NewWeight /> */}
 
-                {/* <div className="weight-container">
-                <p className="item title">Date</p>
-                <p className="item title">Weight</p>
-                <p className="item title">BMI</p>
+                <div className="init-profile-form">
+                    <div>
+                        <label htmlFor="weight">My new weight:</label>
+                        <input type="number" id="weight" placeholder="60" step="5" />
+                    </div>
+                </div>
+
+                <button >Add weight</button>
+                <button><NavLink to={"/weight"} className={"logs"}>My weight log</NavLink></button>
+
                 <hr />
+                <div className="init-profile-form">
+                    <div>
+                        <label htmlFor="activity">New activity:</label>
+                        <input type="text" id="activity" placeholder="Enter new activity" />
+                    </div>
 
-                {weightList.map((e)=> {
-                    return (
-                        <> <p className="item ">{e.date.split("T")[0]}</p>
-                        <p className="item ">{e.weight}</p>
-                        <p className="item ">{e.BMI}</p>
-                        </>
-                    )
-                }
-                )}
-            </div> */}
-                <hr />
-
-                <NewActivity />
+                    <div>
+                        <label htmlFor="duration">Duration in minutes:</label>
+                        <input type="number" id="duration" placeholder="30" step="5" />
+                    </div>
+                </div>
                 <button>Add new activity</button>
-                <button>My activities</button>
+                <button><NavLink to={"/fitness"} className={"logs"}>My activities</NavLink></button>
             </div>
         </div>
 
