a9de09c2d66ed2c7bd0a6bf5bc8f1b0db56775e0
jelena2803 <jelenaiva2803@gmail.com>
2024-03-12 00:30:53 +0200

    credentials added to cors, schemas moved to separate files, controllers created

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,12 @@ app.use(express.json());
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors({ origin: true, credentials: true }));
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const adminController = require("./controllers/adminController");
+const fitnessController = require("./controllers/fitnessController");
+const weightController = require("./controllers/weightController");
 
 main()
   .then(() => console.log("DB connected successfully"))
@@ -20,86 +19,10 @@ async function main() {
   await mongoose.connect(process.env.URL);
 }
 
-const Users = mongoose.model("Users", {
-  username: String,
-  password: String,
-  age: Number,
-  gender: String,
-  height: Number,
-});
-
-const weightSchema = mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: "Users",
-    },
-    date: String,
-    weight: Number,
-    BMI: Number,
-  },
-  { timestamps: true }
-);
-
-const Weight = mongoose.model("Weight", weightSchema);
-
-const fitnessSchema = mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: "Users",
-    },
-    date: String,
-    activity: String,
-    duration: Number,
-  },
-  { timestamps: true }
-);
-const Fitness = mongoose.model("Fitness", fitnessSchema);
-
-function validateToken(req, res, next) {
-  // check if the user has a valid token and if it valid
-  const authHeader = req.headers.authorization;
-  // console.log(authHeader);
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    // var result = jwt.verify(token, "tracker");
-    console.log("hi", token);
-    jwt.verify(token, "tracker", (err, result) => {
-      if (err) {
-        res.status(401).json({ msg: "you are not authorized to this request" });
-      } else {
-        console.log("this is ", result);
-        next();
-      }
-    });
-  } else {
-    res.status(401).json({ msg: "token not found" });
-  }
-
-  // const authHeader = req.headers.authorization;
-  // console.log(authHeader);
-
-  // if (authHeader) {
-  //   const token = authHeader.split(" ")[1];
-
-  //   try {
-  //     const decoded = jwt.verify(token, "tracker");
-  //     req.user = decoded; // The decoded payload will be available in req.user
-
-  //     next();
-  //   } catch (error) {
-  //     console.log(error);
-  //     return res.status(403).json({ error: "Failed to authenticate token." });
-  //   }
-  // } else {
-  //   res.status(401).json({ error: "No token provided." });
-  // }
-}
-
 // get all fitness data
 app.get("/fitness", async (req, res) => {
   try {
+    console.log(req);
     const allActivities = await Fitness.find().sort({ createdAt: -1 });
     res.send(allActivities);
     console.log("get fitness passed");
@@ -113,7 +36,7 @@ app.get("/fitness", async (req, res) => {
 // get all users
 app.get("/", async (req, res) => {
   try {
-    const allUsers = await Users.find({});
+    const allUsers = await Admin.find({});
     res.send(allUsers);
   } catch (error) {
     // Handle any errors that may occur during the creation process
@@ -122,60 +45,13 @@ app.get("/", async (req, res) => {
 });
 
 // new user upon signup
-app.post("/", async (req, res) => {
-  try {
-    // check if both username and password are sent
-    if (!req.body.username || !req.body.password) {
-      res.send({ msg: "please fill the missing data of the user" });
-    }
-    // check if the same username already exists in th db, if not, create new user in db
-    const checkUser = await Users.findOne({ username: req.body.username });
-    if (checkUser !== null) {
-      res.send({
-        msg: "username already exists, you need to use different username",
-      });
-    } else {
-      // hash the password with bcrypt
-      bcrypt.hash(req.body.password, 10, async (err, hash) => {
-        let newUser = {
-          username: req.body.username,
-          password: hash,
-          age: req.body.age,
-          gender: req.body.gender,
-          height: req.body.height,
-        };
-        await Users.create(newUser);
-        res.send({ msg: "user created successfully" });
-      });
-    }
-  } catch (error) {
-    // Handle any errors that may occur during the creation process
-    res.status(500).json({ error: "Failed to create a new user" });
-  }
-});
+app.post("/", adminController.signup);
 
 //add more info of the new user when signup initiated with email and password
-app.put("/newprofile/:id", async (req, res) => {
-  try {
-    const updatedNewUser = await Users.findOneAndUpdate(
-      { _id: req.params.id },
-      req.body
-      // req.body.status == "pending" ? "done" : req.body.status
-    );
-    // Return the message if user is updated successfully
-    const updatedNewUserFinal = await Users.findOne({ _id: req.params.id });
-    res.send(updatedNewUserFinal);
-  } catch (error) {
-    // Handle any errors that may occur during the creation process
-    res.status(500).json({ error: "Failed to update a new user" });
-  }
-});
+app.put("/newprofile/:id", adminController.completeProfile);
 
 // show all the user's profile when profile is completed
-app.get("/newprofile/:id", async (req, res) => {
-  const userProfileCompleted = await Users.findOne({ _id: req.params.id });
-  res.send(userProfileCompleted);
-});
+app.get("/newprofile/:id", adminController.getFullProfile);
 
 // app.get("/profile/:id", async (req, res) => {
 //   let userId = req.params.id;
@@ -183,34 +59,7 @@ app.get("/newprofile/:id", async (req, res) => {
 //   res.send(user);
 // });
 
-// log in process request
-app.post("/login", async (req, res) => {
-  try {
-    // check if both username and password are sent
-    if (!req.body.username || !req.body.password) {
-      res.send({ msg: "please fill the missing data uf the user" });
-    }
-
-    // check if the username is correct
-    let user = await Users.findOne({ username: req.body.username });
-    if (user === null) {
-      res.send({ msg: "Wrong username, enter again your username" });
-    } else {
-      // if username exists, we check the password
-      bcrypt.compare(req.body.password, user.password, (err, result) => {
-        if (result) {
-          var token = jwt.sign({ id: user._id }, "tracker");
-          res.send({ user: user, token: token });
-        } else {
-          res.send({ msg: "Wrong password, enter again your password" });
-        }
-      });
-    }
-  } catch (error) {
-    // Handle any errors that may occur during the creation process
-    res.status(500).json({ error: "Failed to create a new user" });
-  }
-});
+app.post("/login", adminController.login);
 
 // render all weight logs
 app.get("/weight", async (req, res) => {
